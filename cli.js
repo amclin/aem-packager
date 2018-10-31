a4189b20ce7adb1f929d35d997d7fda79852973e
Anthony McLin <anthony.mclin@rosetta.com>
2018-10-30 19:30:19 -0700

    stub in basic pom.xml to build AEM package in project's target directory

--- cli.js
+++ cli.js
@@ -1,6 +1,50 @@
 #!/usr/bin/env node
 
-// Get command line arguments
-const [,, ...args] = process.argv
+const [,, ...args] = process.argv // Get command line arguments
+const path = require('path')
+
+// Define paths used in various steps of the process
+const paths = {
+  pom: path.resolve(__dirname, 'src/pom.xml'),
+  mvnTarget: path.resolve(process.cwd(), 'target'),
+  npmOut: path.resolve(process.cwd(), 'dist')
+}
+
+// Define defaults when configs are not provided
+const defaults = {
+  maven: {
+    commands: [
+      '-f',
+      paths.pom,
+      'clean',
+      'install',
+      '-Pnpm' // Force a build profile that lets us set the Maven build target folder
+    ],
+    defines: {
+      npmName: 'My Project',
+      npmDescription: 'My Project Description',
+      npmGroupId: 'myGroupId',
+      npmArtifactId: 'myArtifactId',
+      npmVersion: '0.0.1',
+      npmBuildOutput: paths.npmOut,
+      npmBuildTarget: paths.mvnTarget,
+      npmJcrPath: 'etc/designs/myGroupId/myArtifactId'
+    },
+    options: {}
+  }
+}
+const config = Object.assign({}, defaults)
+const mvn = require('maven').create(config.maven.options)
 
 console.log(`Running AEM Packager with arguments ${args}`)
+console.log(`Running AEM Packager with defaults ${defaults}`)
+
+mvn.execute(
+  config.maven.commands,
+  config.maven.defines
+).then(result => {
+  console.log(`AEM package has been created and can be found in ${paths.mvnTarget}`)
+}).catch(result => {
+  console.error('Failed to compile Maven package. See Maven log for details.')
+  process.exit(1)
+})
