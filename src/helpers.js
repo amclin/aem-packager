0b42a13ec0b98ed7a3b0e0a2a7c0eb3b1b36ebdc
Anthony McLin <anthony.mclin@rosetta.com>
2018-11-05 11:15:46 -0800

    private helper function to make it easier to get keys from process.env

--- src/helpers.js
+++ src/helpers.js
@@ -15,14 +15,25 @@ const prefixProperties = function (obj, prefix) {
 }
 
 /**
+ * Gets a specific property from the NPM process.env
+ * dashes in search segments will be converted to underscore
+ * @param {Array} searchPath - Array defining the path to the property
+ */
+const getFromEnv = function (searchPath) {
+  var key = searchPath.join('_').replace('-', '_')
+  return process.env[key]
+}
+
+/**
  * Gets an NPM package.json property by name
  * This is shorthand for process.env.npm_package_<name>
  * @param {String} name
  * @param {String} prefix - Optionap prefix for finding an option in the namespace
  */
 const getNPM = function (name, prefix) {
-  prefix = prefix || 'npm_package_'
-  return process.env[`${prefix}${name}`]
+  prefix = prefix || 'npm_package'
+  prefix = prefix.replace(/_$/, '') // remove trailing _ if there is one
+  return getFromEnv([prefix, name])
 }
 
 module.exports.prefixProperties = prefixProperties
